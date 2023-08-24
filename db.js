f93ff32a5a26edd10322c38e11f5a168e1ab7c82
priyansu-nayak <priyansu18.8@gmail.com>
2023-08-24 12:07:48 +0530

    updated

--- db.js
+++ db.js
@@ -2,3 +2,8 @@
 const mongoose = require('mongodb://localhost:27017/')
 const mongoURI = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
 
+const connectToMongo = () =>{
+    mongoose.connect(mongoURI,()=>{
+        console.log("Connected to Mongo Successfully")
+    })
+}
