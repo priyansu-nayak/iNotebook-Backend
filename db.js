6bdc244581b14aa64809aeb14ff0ebb0f5554df1
priyansu-nayak <priyansu18.8@gmail.com>
2023-08-24 11:58:46 +0530

    updated

---
+++ db.js
@@ -0,0 +1,4 @@
+//es6 modules are not being used, common modules are being used here in nodejs
+const mongoose = require('mongodb://localhost:27017/')
+const mongoURI = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
+
