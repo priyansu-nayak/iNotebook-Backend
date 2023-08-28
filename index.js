b19a541c8c8f52ec084a90aee59dca5dbeef72f9
priyansu-nayak <priyansu18.8@gmail.com>
2023-08-28 22:23:56 +0530

    updated

--- index.js
+++ index.js
@@ -17,8 +17,10 @@ const port = 3000
 //   res.send('Hello signup!')
 // })
 
+
+//Available Routes
 app.use('/api/auth',require('./routes/auth'))
-app.use('/api/notes',require('./routes/notes'))
+// app.use('/api/notes',require('./routes/notes'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port http:localhost:${port}`)
