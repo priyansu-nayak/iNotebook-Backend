4bb9fca16eef71f597e2c0fa67e2b177a60f03ab
priyansu-nayak <priyansu18.8@gmail.com>
2023-08-28 22:34:40 +0530

    updated

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const port = 3000
 
 //Available Routes
 app.use('/api/auth',require('./routes/auth'))
-// app.use('/api/notes',require('./routes/notes'))
+app.use('/api/notes',require('./routes/notes'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port http:localhost:${port}`)
