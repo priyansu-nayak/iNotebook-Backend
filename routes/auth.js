9778637f7842fe3958f2a02edc418e3bfb1f4d46
priyansu-nayak <priyansu18.8@gmail.com>
2023-08-29 22:24:07 +0530

    updated

--- routes/auth.js
+++ routes/auth.js
@@ -2,11 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    const obj = {
-        a: 'thios',
-        number: 34
-    }
-    res.json(obj)
+    
+    res.json(req.body)
 })
 
 module.exports = router
