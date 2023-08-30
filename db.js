65f255aa65126c4732a436c34cb1b132f6a0d69c
priyansu-nayak <priyansu18.8@gmail.com>
2023-08-30 12:47:17 +0530

    updated

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 //es6 modules are not being used, common modules are being used here in nodejs
 const mongoose = require('mongoose')
-const mongoURI = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
+const mongoURI = "mongodb://localhost:27017/"
+// const mongoURI = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
 
 const connectToMongo = () =>{
     mongoose.connect(mongoURI,()=>{
