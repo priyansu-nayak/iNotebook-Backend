e790c46c090d1b550063cf4122990aefd42797ad
priyansu-nayak <priyansu18.8@gmail.com>
2023-08-30 11:39:13 +0530

    updated

--- index.js
+++ index.js
@@ -5,19 +5,6 @@ connectToMongo();
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.get('/api/v1/login',(req,res)=>{
-  res.send('Hello login!')
-})
-
-app.get('/api/v1/signup',(req,res)=>{
-  res.send('Hello signup!')
-})
-
-
 //Available Routes
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/notes',require('./routes/notes'))
