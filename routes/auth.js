7eba96dcbe47c8d1030d96b9d5cba36dee4d743a
priyansu-nayak <priyansu18.8@gmail.com>
2023-08-30 12:19:08 +0530

    updated

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User');
+
+//Create a User using:POST "/api/auth/". Doesn't require Auth
 
 router.get('/', (req, res) => {
     console.log(req.body)
+    res.send(req.body);
 })
 
 module.exports = router
